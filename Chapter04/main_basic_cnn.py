492176d3ddd9c249621cd094067c48e5f6b9f7dd
JUNG SEONG DU <156891260+JungSeongDu@users.noreply.github.com>
2024-04-11 22:04:38 +0900

    Update main_basic_cnn.py

--- Chapter04/main_basic_cnn.py
+++ Chapter04/main_basic_cnn.py
@@ -3,11 +3,29 @@ import random
 import warnings
 warnings.filterwarnings("ignore")
 from utils import train_test_split
+
+
+src = 'Chapter04/Dataset/kagglecatsanddogs_5340/PetImages/'
+
+# 데이터셋이 있는지 먼저 확인한다. 없다면 내려 받아야 한다
+if not os.path.isdir(src):
+    print("""
+          Dataset not found in your computer.
+          Please follow the instructions in the link below to download the dataset:
+          https://raw.githubusercontent.com/PacktPublishing/Neural-Network-Projects-with-Python/master/chapter4/how_to_download_the_dataset.txt
+          """)
+    quit()
+
+# train/test 폴더가 없다면 생성한다
+if not os.path.isdir(src+'train/'):
+    train_test_split(src)
+
 from keras.models import Sequential
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
 
 # 초매개변수 정의
 FILTER_SIZE = 3
@@ -30,6 +48,8 @@ model.add(Flatten())
 model.add(Dense(units = 128, activation = 'relu'))
 model.add(Dropout(0.5))
 model.add(Dense(units = 1, activation = 'sigmoid'))
+
+
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 training_data_generator = ImageDataGenerator(rescale = 1./255)
@@ -46,9 +66,14 @@ test_set = testing_data_generator.flow_from_directory('Chapter04/Dataset/kagglec
                                              batch_size = BATCH_SIZE,
                                              class_mode = 'binary')
 
-model.fit_generator(training_set, steps_per_epoch = STEPS_PER_EPOCH, epochs = EPOCHS, verbose=1)
 
-score = model.evaluate_generator(test_set, steps=100)
+model.fit(training_set, steps_per_epoch = STEPS_PER_EPOCH, epochs=EPOCHS, verbose=1)
+score = model.evaluate(test_set, steps=100)
 
 for idx, metric in enumerate(model.metrics_names):
     print("{}: {}".format(metric, score[idx]))
+
+
+# 모델 저장
+model.save('main_basic_cnn.h5')
+
